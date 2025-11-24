f2d042ae8e7880f77e637d5d38996a46302a66c9
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-11-24 13:25:05 +0100

    No real changes

--- frontend/src/components/TextDispayer.tsx
+++ frontend/src/components/TextDispayer.tsx
@@ -6,10 +6,13 @@ interface TextProps {
 
 class TextRender extends React.Component<TextProps>{
     render(): React.ReactNode {
-        let website = fetch(this.props.url)
-        
-        console.log(website.finally.toString)
-        return 
+        let website = fetch(this.props.url).then(response => {(response.text);
+            return response.text
+        });
+        //let website = fetch(this.props.url)
+
+        console.log(website)
+        return <div></div>
     }
 }
 
