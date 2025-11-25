f24786c256ea4df99168aab07859bf35f68556ba
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-11-25 12:17:50 +0100

    Included PowerPoint

--- frontend/src/components/TextDispayer.tsx
+++ frontend/src/components/TextDispayer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GetExternal } from "../services/apiService";
+import parse from 'html-react-parser';
 
 interface TextProps {
     url: string
@@ -8,13 +9,21 @@ interface TextProps {
 
 class TextRender extends React.Component<TextProps>{
     fetchData = async() => {
-        let res = await GetExternal(this.props.url, this.props.tutorial);
-        let data = res
-        console.log(data)
+        const res = await GetExternal(this.props.url, this.props.tutorial);
+        const htmlString = await res.text;
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlString, "application/xhtml+xml");
+
+        // Example: Extract all text from the body
+        const extractedText = doc.body.textContent.toString();
+        console.log("All extracted text")
+        console.log(extractedText)
+        console.log(parse(extractedText))
+        return parse(extractedText)
 
     }
     render() {
-        this.fetchData()
+        const html = this.fetchData()
         return <div></div>
     }
 }
