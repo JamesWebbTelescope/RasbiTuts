7dde1806b1f5b4d674a1985a5a2de922e8c42ba9
JamesWebbTelescope <Viktorfrom@gmail.com>
2026-01-04 17:19:14 +0100

    Update navigation links to use relative paths
    Changed navigation links in the frontend to use relative paths (e.g., './pages/students' instead of '/pages/students') for improved routing compatibility. Updated corresponding references in built HTML and JS files.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -52,28 +52,28 @@ export default function Navbar() {
       <a href="/RasbiTuts/" className={holoLinkClass}>
         Home
       </a>
-      <a href="/pages/students" className={holoLinkClass}>
+      <a href="./pages/students" className={holoLinkClass}>
         Students
       </a>
       {
         <button
           onClick={() => {
             // clearToken();
-            navigate("/pages/students");
+            navigate("./pages/students");
           }}
           className={holoLinkClass}
         >
           {}
         </button>
       }
-      <a href="/pages/tutorials" className={holoLinkClass}>
+      <a href="./pages/tutorials" className={holoLinkClass}>
         Tutorials
       </a>
       {
         <button
           onClick={() => {
             // clearToken();
-            navigate("/pages/tutorials");
+            navigate("./pages/tutorials");
           }}
           className={holoLinkClass}
         >
