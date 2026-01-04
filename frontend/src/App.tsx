43790b973af22371fd22bf3917cddf42eb288b8d
JamesWebbTelescope <Viktorfrom@gmail.com>
2026-01-04 16:59:22 +0100

    Tried fixing header routes again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,8 @@ export default function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/students" element={<StudentsPage/>}/>
-        <Route path="/tutorials" element={<TutorialsPage/>}/>
+        <Route path="/RasbiTuts/students" element={<StudentsPage/>}/>
+        <Route path="/RasbiTuts/tutorials" element={<TutorialsPage/>}/>
       </Routes>
     </HashRouter>
   );
